0974081659716b248e40e5b2eefe22ad8eb0e82d
MrX-zeta <brianluisruizperez@gmail.com>
2026-02-03 23:34:24 -0600

    Inicialización del microservicio de Usuarios

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+
+  const logger = new Logger('Auth-MS')
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options:{
+        port: 3001
+      }
+    }
+  );
+  await app.listen();
+  logger.log('Microservice is listening on port 3001');
+}
+bootstrap();

852e573e967deec60604871b13779b8d39152c21
MrX-zeta <brianluisruizperez@gmail.com>
2026-02-28 00:42:17 -0600

    fix import name

--- src/password-resets/password-resets.module.ts
+++ src/password-resets/password-resets.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PasswordResetsService } from './services/password-resets.service';
 import { PasswordResetsSenderService } from './services/password-resets-sender.service';
+import { PasswordResetTokenReceiverService } from './services/password-reset-token-receiver.service';
 import { PasswordResetsController } from './password-resets.controller';
 
 @Module({
   controllers: [PasswordResetsController],
-  providers: [PasswordResetsService, PasswordResetsSenderService],
+  providers: [PasswordResetsSenderService, PasswordResetTokenReceiverService],
 })
 export class PasswordResetsModule {}
